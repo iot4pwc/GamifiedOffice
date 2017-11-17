4bbf40eea22a7f68ec4fdf84ec8d1b6abab9e111
Stefan Hermanek <hermanek.stefan@gmail.com>
2017-11-17 13:57:14 -0500

    SOB type of commit. Total Savage.

--- gamiOffice/src/main/java/gamiOffice/components/activities/WaterIntake.java
+++ gamiOffice/src/main/java/gamiOffice/components/activities/WaterIntake.java
@@ -4,6 +4,8 @@ import gamiOffice.components.general.User;
 
 public class WaterIntake extends Activity{
 
+	public static final String COMPONENT_CODE = "WaterIntake";
+	
 	@Override
 	public double calculateScore(User u) {
 		// TODO Auto-generated method stub
--- gamiOffice/src/main/java/gamiOffice/components/general/User.java
+++ gamiOffice/src/main/java/gamiOffice/components/general/User.java
@@ -5,52 +5,60 @@ import java.util.*;
 import gamiOffice.components.activities.Activity;
 
 public class User {
-	//Employee basic information
-	private String EmploeeId;
-	private String Name;
-	private String Email;
-	private String Alias;
-	
-	//Map storing the activity the employee is enrolled and the corresponding sensor id
-	Map<Activity, List<String>> ActivityMap;
-	/**
-	 * @param String name - name of employee
-	 * @param String email - email of employee
-	 * @param String employeeid - id of employee
-	 * 
-	 * The name, email and id can only be set when initialize*/
-	User(String name, String email, String employeeid){
-		this.Name = name;
-		this.Email = email;
-		this.EmploeeId = employeeid;
-		this.setAlias("Not Specified");
-		this.ActivityMap = new HashMap<Activity, List<String>>();
-	}
+  //Employee basic information
+  private String EmployeeId;
+  private String Name;
+  private String Email;
+  private String Alias;
 
-	/**
-	 * Getter method for all parameters
-	 * */
-	public String getEmploeeId() {
-		return EmploeeId;
-	}
+  //Map storing the activity the employee is enrolled and the corresponding sensor id
+  Map<Activity, List<String>> ActivityMap;
+  /**
+   * @param String name - name of employee
+   * @param String email - email of employee
+   * @param String employeeid - id of employee
+   * 
+   * The name, email and id can only be set when initialize*/
+  User(String name, String email, String employeeid){
+    this.Name = name;
+    this.Email = email;
+    this.EmployeeId = employeeid;
+    this.setAlias("Not Specified");
+    this.ActivityMap = new HashMap<Activity, List<String>>();
+  }
 
-	public String getName() {
-		return Name;
-	}
+  User(String name, String email, String employeeid, String alias){
+    this.Name = name;
+    this.Email = email;
+    this.EmployeeId = employeeid;
+    this.setAlias(alias);
+    this.ActivityMap = new HashMap<Activity, List<String>>();
+  }
 
-	public String getEmail() {
-		return Email;
-	}
+  /**
+   * Getter method for all parameters
+   * */
+  public String getEmploeeId() {
+    return EmployeeId;
+  }
 
-	public String getAlias() {
-		return Alias;
-	}
+  public String getName() {
+    return Name;
+  }
 
-	/**
-	 * Setter method for Alias
-	 * */
-	public void setAlias(String alias) {
-		Alias = alias;
-	}
+  public String getEmail() {
+    return Email;
+  }
+
+  public String getAlias() {
+    return Alias;
+  }
+
+  /**
+   * Setter method for Alias
+   * */
+  public void setAlias(String alias) {
+    Alias = alias;
+  }
 
 }
